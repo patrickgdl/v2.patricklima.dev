baacff0dbaae4dc19b6f6ef2f6b3a390adda5182
Patrick Lima <patrick.lima@picpay.com>
2022-01-26 21:39:37 -0300

    feat: lint + tests + misc pkgs

---
+++ next.config.js
@@ -0,0 +1,53 @@
+/** @type {import('next').NextConfig} */
+const withPlugins = require('next-compose-plugins');
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = {
+  swcMinify: true,
+  reactStrictMode: true,
+  env: {
+    WEATHER_API_KEY: process.env.WEATHER_API_KEY,
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+  },
+  images: {
+    domains: ['openweathermap.org'],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            prettier: false,
+            svgo: false,
+            titleProp: true,
+          },
+        },
+        {
+          loader: 'file-loader',
+          options: {
+            name: 'static/media/[name].[hash].[ext]',
+          },
+        },
+      ],
+      issuer: {
+        and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
+      },
+    });
+    return config;
+  },
+};
+module.exports = withPlugins([withBundleAnalyzer], nextConfig);
